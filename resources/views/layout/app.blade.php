73732765981a325a101591a5af66001ad2c3d90e
paras1729kori <paras1799kori@gmail.com>
2022-01-18 22:23:43 +0530

    changes in app

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -22,6 +22,7 @@
 
 </head>
 <body style="--tw-bg-opacity: 1; background-color: rgba(229, 231, 235, var(--tw-bg-opacity));">
+    <h1>hero</h1>
     {{-- Navbar --}}
     <nav style="background-color:rgb(128,33,33)" class="navbar navbar-expand-lg sticky-top">
         <a class="navbar-brand font-weight-bold" href="{{ route('home') }}">KJSIEIT</a>
