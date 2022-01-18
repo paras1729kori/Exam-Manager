fb155ec26917323866f6afbdde51f09ddf5a8fd1
unknown <vishesh.k@somaiya.edu>
2022-01-19 01:00:58 +0530

    external fac det and published notice

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,13 +4,59 @@
 
     <div class="d-flex flex-column align-items-center justify-content-center">
         <div class="bg-light px-5 w-100 py-3 text-center mb-2">
-            <h3 class="font-weight-bold">Admin Dashboard</h3>
+            <div class="flex-row align-items-center justify-content-center">
+                <h3 class="font-weight-bold">Admin Dashboard</h3>
+                    <button type="button" class="btn btn-warning">Forget password</button>
+                
+            </div>
+            
+            
         </div>
 
-        forgot password section
+        {{-- forgot password section
         posted notices [Delete button]
         name
-        department
+        department --}}
+        <div class="row mb-5 text-center w-100">
+            <div class="col-12">
+                <h4>Notices Published</h4>
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col">
+                        <div class="list-group-item list-group-item-action">
+                            <a href=" " class="font-weight-bold" style="color:rgb(128,33,33)">Technical festival Renaissance 2021-22"</a>
+                        <a href="" class="text-dark d-flex justify-content-end">Delete</a>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col">
+                        <div class="list-group-item list-group-item-action">
+                            <a href=" " class="font-weight-bold" style="color:rgb(128,33,33)">Technical festival Renaissance 2021-22"</a>
+                        <a href="" class="text-dark d-flex justify-content-end">Delete</a>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+            <div class="col-12">
+                <div class="row">
+                    <div class="col ">
+                        <div class="list-group-item list-group-item-action">
+                            <a href=" " class="font-weight-bold" style="color:rgb(128,33,33)">Technical festival Renaissance 2021-22"</a>
+                            <button type="button" class="btn btn-danger">Delete</button>
+                        </div>
+                    </div>
+                </div>
+               
+            </div>
+            
+          </div>
+          </div>
     </div>
 
 @endsection
